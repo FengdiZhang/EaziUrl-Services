1d92dde26662ba1796f5dee1fcd2d54250c11983
FengdiZhang <faydi.mu06@gmail.com>
2024-06-12 13:16:10 -0700

    change before merge

--- api/models.py
+++ api/models.py
@@ -1,8 +1,10 @@
+#models.py
 from django.db import models
 
 class URLMapping(models.Model):
     long_url = models.URLField()
     short_url = models.CharField(max_length=10, unique=True)
+    title = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
