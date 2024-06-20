ab857790e5c5ee22c5cef9206568aa8aeddb974d
FengdiZhang <faydi.mu06@gmail.com>
2024-06-20 16:23:10 -0700

    add authentication/auth.py

--- api/auth.py
+++ api/auth.py
@@ -5,19 +5,20 @@ from passlib.context import CryptContext
 from .models import UserInDB, TokenData
 from fastapi import HTTPException, status
 
-# os checkout later --
-SECRET_KEY = "mysecretkey"
+SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# verify pwd
+# verify password
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
-# get pwd
+
+# get password hash
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
 # create JWT token
 def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
@@ -25,11 +26,17 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None) -> s
         expire = datetime.utcnow() + expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
-        to_encode.update({"exp": expire})
-        encoded = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-        return encoded
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
 # decode/verify JWT token
 def decode_access_token(token: str):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
@@ -39,7 +46,3 @@ def decode_access_token(token: str):
     except JWTError:
         raise credentials_exception
     return token_data
-
-
-
-
