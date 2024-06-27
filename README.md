1ebe9bc88f5d56aabf2bf817795a4f424c072696
FengdiZhang <faydi.mu06@gmail.com>
2024-06-27 14:09:58 -0700

    add OAuth2

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 ## Overview
 URL Shortener Backend Services built using Django and FastAPI. The backend is powered by Django for database management and FastAPI for handling API requests. PostgreSQL is used as the database, and Redis is used for caching.
 
+**Frontend ➡️ https://github.com/FengdiZhang/EaziUrl-UI**
 
 ## Requirements
 - Python 3.12
@@ -12,21 +13,51 @@ URL Shortener Backend Services built using Django and FastAPI. The backend is po
 
 
 ## Before start:
-- Run Docker Desktop 
-- Add docker-compose.yml file
-- Run Redis Insight 
+- Run Docker Desktop: Make sure Docker Desktop is running on your machine.
+- Add docker-compose.yml file: Ensure you have a docker-compose.yml file in your project directory.
+- Run Redis Insight: Make sure Redis Insight is running to monitor your Redis database.
 
 ## Get started:
 
-- docker-compose up --build
-- docker exec -it <container_id>
-- docker-compose exec python manage.py createsuperuser
-- python -m venv venv
-- source venv/bin/activate
-- pip install django fastapi uvicorn django-redis pydantic python-jose passlib
-- python manage.py makemigrations
-- python manage.py migrate
-- uvicorn myproject.asgi:app --reload
+
+1. **Build and start Docker containers**:
+   ```sh
+   docker-compose up --build
+- Build the Docker images and starts the containers defined in your docker-compose.yml file. It sets up PostgreSQL and Redis, along with your Django and FastAPI services.
+2. **List your running containers:**:
+   ```sh
+   docker ps
+- Lists all running Docker containers. Note the <container_id> of your Django container.
+3. **Access the running container's shell::**:
+   ```sh
+   docker exec -it <container_id> /bin/bash
+- Replace <container_id> with the ID of your Django container. This command gives you access to the shell of the running container.
+4. **Create a Django Superuser:**
+   ```sh
+   python manage.py createsuperuser
+
+- Creates a superuser for the Django admin panel. You will be prompted to enter a username, email, and password.
+5. **Set Up a Virtual Environment:**
+    ```sh
+   python -m venv venv
+- Creates a virtual environment named venv
+6. **Activate the virtual environment:**
+    ```sh
+   source venv/bin/activate
+- Activates the virtual environment.
+7. **Install required packages:**
+    ```sh
+   pip install django fastapi uvicorn django-redis pydantic python-jose passlib authlib itsdangerous python-dotenv PyJWT
+- Installs all the necessary packages for your project.
+8. **Make and apply migrations:**
+    ```sh
+   python manage.py makemigrations
+    python manage.py migrate
+- Create and apply database migrations, setting up your database schema.
+9. **Start the FastAPI server:**
+   ```sh 
+   uvicorn myproject.asgi:app --reload
+- Starts the FastAPI server and reloads it automatically when code changes are detected.
 
 ## Access application:
 - FastAPI documentation: http://127.0.0.1:8000/api/docs
--- api/dependency.py
+++ api/dependency.py
@@ -1,11 +1,68 @@
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+import os
+from fastapi import Depends, FastAPI, HTTPException, status, Request
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.responses import RedirectResponse
+from datetime import timedelta
+from typing import Optional
 from jose import JWTError
-from .auth import decode_access_token
-from .models import TokenData, User, CustomUser
-from asgiref.sync import sync_to_async
+from authlib.integrations.starlette_client import OAuth
+from starlette.middleware.sessions import SessionMiddleware
+from dotenv import load_dotenv
 
+from .models import CustomUser, Token, UserCreate, User
+from django.db import transaction
+from asgiref.sync import sync_to_async
+from .auth import (
+    verify_password,
+    get_password_hash,
+    create_access_token,
+    decode_access_token
+)
+
+load_dotenv()
+
+auth_app = FastAPI()
+auth_app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"))
+
+# OAuth2 configuration
+client_id = os.getenv("GOOGLE_CLIENT_ID")
+client_secret = os.getenv("GOOGLE_CLIENT_SECRET")
+
+oauth = OAuth()
+oauth.register(
+    name='google',
+    client_id=client_id,
+    client_secret=client_secret,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    access_token_url='https://oauth2.googleapis.com/token',
+    access_token_params=None,
+    refresh_token_url=None,
+    redirect_uri='http://127.0.0.1:8000/auth/callback',
+    client_kwargs={
+        'scope': 'openid profile email',
+    },
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration'
+)
+
+# Define the constant
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
+
+async def get_user(username: str):
+    try:
+        user = await sync_to_async(CustomUser.objects.get)(username=username)
+        return user
+    except CustomUser.DoesNotExist:
+        return None
+
+async def authenticate_user(username: str, password: str):
+    user = await get_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return user
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -15,10 +72,10 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     )
     try:
         payload = decode_access_token(token)
-        username = payload.username
+        username = payload["sub"]
     except JWTError:
         raise credentials_exception
-    user = await sync_to_async(CustomUser.objects.get)(username=username)
+    user = await get_user(username=username)
     if user is None:
         raise credentials_exception
     return user
@@ -27,3 +84,72 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
+
+@sync_to_async
+def create_user_in_db(username: str, password: str, email: str):
+    with transaction.atomic():
+        return CustomUser.objects.create(
+            username=username,
+            password=get_password_hash(password),
+            email=email
+        )
+
+# ⛳️ all auth_endpoints:
+@auth_app.post("/register")
+async def register_user(user: UserCreate):
+    new_user = await create_user_in_db(user.username, user.password, user.email)
+    return {"message": "User registered successfully"}
+
+@auth_app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = await authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@auth_app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+# OAuth2 login
+@auth_app.get("/login")
+async def login_via_google(request: Request):
+    redirect_uri = 'http://127.0.0.1:8000/auth/callback'
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+# OAuth2 callback
+@auth_app.route("/callback")
+async def auth_callback(request: Request):
+    try:
+        token = await oauth.google.authorize_access_token(request)
+        if 'id_token' not in token:
+            raise HTTPException(status_code=400, detail="ID token is missing")
+
+        user_info = await oauth.google.parse_id_token(request, token)
+        print("User Info:", user_info)  # Add this line for debugging
+
+        username = user_info['email']
+        email = user_info['email']
+
+        user = await get_user(username)
+        if not user:
+            user = await create_user_in_db(username=username, password='', email=email)
+
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": user.username}, expires_delta=access_token_expires
+        )
+        response = RedirectResponse(url='/')
+        response.set_cookie(key="Authorization", value=f"Bearer {access_token}", httponly=True)
+        return response
+    except Exception as e:
+        print("Error in callback:", str(e))
+        raise HTTPException(status_code=500, detail=str(e))
